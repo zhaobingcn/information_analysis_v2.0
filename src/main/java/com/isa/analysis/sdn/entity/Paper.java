c78d70dc3d90380b700fbfc9e5fd8afeec615aed
zhaobingcn <zhaobingcn@hotmail.com>
2016-12-30 22:43:47 +0800

    12.30.22.43

---
+++ src/main/java/com/isa/analysis/sdn/entity/Paper.java
@@ -0,0 +1,7 @@
+package com.isa.analysis.sdn.entity;
+
+/**
+ * Created by zhzy on 2016/12/30.
+ */
+public class Paper {
+}
---
+++ src/main/java/com/isa/analysis/sdn/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.isa.analysis.sdn.repository;
+
+/**
+ * Created by zhzy on 2016/12/30.
+ */
+public class AuthorRepository {
+}
