bec8583f2c10b7251d436e54ea108954b38f6d90
zhaobingcn <zhaobingcn@hotmail.com>
2016-12-30 23:02:15 +0800

    12.30.23.02

--- src/main/java/com/isa/analysis/sdn/entity/Institution.java
+++ src/main/java/com/isa/analysis/sdn/entity/Institution.java
@@ -3,5 +3,5 @@ package com.isa.analysis.sdn.entity;
 /**
  * Created by zhzy on 2016/12/30.
  */
-public interface Institution {
+public class Institution {
 }
--- src/main/java/com/isa/analysis/sdn/repository/PaperRepository.java
+++ src/main/java/com/isa/analysis/sdn/repository/PaperRepository.java
@@ -3,5 +3,5 @@ package com.isa.analysis.sdn.repository;
 /**
  * Created by zhzy on 2016/12/30.
  */
-public class PaperRepository {
+public interface PaperRepository {
 }
