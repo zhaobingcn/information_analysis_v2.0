1a74ae9ab7c521af9a049b1efb9311ac9291e2de
zhaobingcn <zhaobingcn@hotmail.com>
2016-12-31 11:00:58 +0800

    12.31.10.60

--- src/main/java/com/isa/analysis/sdn/repository/JournalRepository.java
+++ src/main/java/com/isa/analysis/sdn/repository/JournalRepository.java
@@ -2,9 +2,11 @@ package com.isa.analysis.sdn.repository;
 
 import com.isa.analysis.sdn.entity.Journal;
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by zhzy on 2016/12/30.
  */
+@Repository
 public interface JournalRepository extends GraphRepository<Journal> {
 }
