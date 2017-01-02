11086bbd31ecae24a548e556b8c5bfc7ae22f4a5
zhaobingcn <zhaobingcn@hotmail.com>
2017-01-02 22:53:58 +0800

    1.2.22.51

--- src/main/java/com/isa/analysis/sdn/entity/WorkTogether.java
+++ src/main/java/com/isa/analysis/sdn/entity/WorkTogether.java
@@ -1,8 +1,57 @@
 package com.isa.analysis.sdn.entity;
 
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.Property;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+import org.neo4j.ogm.annotation.StartNode;
+
 /**
  * Created by zhzy on 2017/1/2.
  */
-
+@RelationshipEntity(type = "work_together")
 public class WorkTogether {
+
+    @GraphId
+    private Long id;
+
+    @Property
+    private Long weight;
+
+    @StartNode
+    private Author author1;
+
+    @StartNode
+    private Author author2;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Long weight) {
+        this.weight = weight;
+    }
+
+    public Author getAuthor1() {
+        return author1;
+    }
+
+    public void setAuthor1(Author author1) {
+        this.author1 = author1;
+    }
+
+    public Author getAuthor2() {
+        return author2;
+    }
+
+    public void setAuthor2(Author author2) {
+        this.author2 = author2;
+    }
 }
