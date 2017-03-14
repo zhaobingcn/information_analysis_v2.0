1a3a8b9997dd5cf0d72a9f8ca8bfaeff993ccc07
zhaobingcn <zhaobingcn@hotmail.com>
2017-03-14 16:52:49 +0800

    3.14.16.52

---
+++ src/main/java/com/isa/analysis/service/ExpertComparisonService.java
@@ -0,0 +1,7 @@
+package com.isa.analysis.service;
+
+/**
+ * Created by zhzy on 17-3-14.
+ */
+public interface ExpertComparisonService {
+}
---
+++ src/main/java/com/isa/analysis/service/impl/ExpertComparsionServiceImpl.java
@@ -0,0 +1,9 @@
+package com.isa.analysis.service.impl;
+
+import com.isa.analysis.service.ExpertComparisonService;
+
+/**
+ * Created by zhzy on 17-3-14.
+ */
+public class ExpertComparsionServiceImpl implements ExpertComparisonService {
+}
