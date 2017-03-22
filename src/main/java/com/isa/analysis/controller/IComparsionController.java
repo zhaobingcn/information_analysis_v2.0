531891944e8ebe4a15015bfe7186b03e9bf66705
zhaobingcn <zhaobingcn@hotmail.com>
2017-03-22 14:48:40 +0800

    3.16.17.00

--- src/main/java/com/isa/analysis/controller/IComparsionController.java
+++ src/main/java/com/isa/analysis/controller/IComparsionController.java
@@ -1,7 +1,15 @@
 package com.isa.analysis.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by zhzy on 17-3-21.
  */
+@Controller
 public class IComparsionController {
+    @RequestMapping(value = "/ComparisonofInstitutions")
+    public String blank(){
+        return "/ComparisonofInstitutions";
+    }
 }
