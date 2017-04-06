75d491d0933fe927b0f6401104363ed51c7e5d0a
zhaobingcn <zhaobingcn@hotmail.com>
2017-04-06 14:57:45 +0800

    4.6.14.20

---
+++ src/main/resources/static/js/expertComparsion/expertRelationshipCompare.js
@@ -0,0 +1,85 @@
+/**
+ * Created by lsy on 2017/2/28.
+ */
+function loadRelationshipExpertCompare(depath) {
+    var myChart = echarts.init(document.getElementById('expert-relationship-comparison'));
+    //myChart.showLoading();
+    $.ajax({
+        url : "/detailOfExpert/cooperateOfAuthor",
+        type: "get",
+        dataType : "json",
+        data:{
+            "name" : "詹毅",
+            "institution" : "电子科技集团36所",
+            "depath" : depath
+        },
+        success : function (graph) {
+            myChart.hideLoading();
+            graph.nodes.forEach(function (node) {
+                node.itemStyle = null;
+                node.value = node.value;
+                node.symbolSize = Math.sqrt(node.value)*7;
+                node.label = {
+                    normal: {
+                        show: node.symbolSize > 5
+                    }
+                };
+                node.category = node.category;
+            });
+            var legendData = [];
+            var categoriesLength = graph.categories.length;
+            for(var i=0; i<categoriesLength; i++){
+                legendData[i] = graph.categories[i].name;
+            }
+            option = {
+                title: {
+                    text: '作者合作关系图',
+                    top: 'bottom',
+                    left: 'right'
+                },
+                tooltip: {},
+                legend: {
+                    data: legendData
+                },
+                animationDurationUpdate: 1500,
+                animationEasingUpdate: 'quinticInOut',
+                series : [
+                    {
+                        name: '专家',
+                        type: 'graph',
+                        layout: 'force',
+                        // circular: {
+                        //     rotateLabel: true
+                        // },
+                        data: graph.nodes.map(function (node, idx) {
+                            node.id = idx;
+                            return node;
+                        }),
+                        links: graph.links,
+                        categories: graph.categories,
+                        roam: true,
+                        label: {
+                            normal: {
+                                position: 'top',
+                                formatter: '{b}'
+                            }
+                        },
+                        lineStyle: {
+                            normal: {
+                                color: 'source',
+                                curveness: 0.0
+                            }
+                        },
+                        edgeSymbol: ['arrow', 'arrow'],
+                        force: {
+                            repulsion: 100,
+                            edgeLength: 100
+                        }
+                    }
+                ]
+            };
+            myChart.setOption(option);
+        }
+    });
+}
+loadRelationshipExpertCompare();
