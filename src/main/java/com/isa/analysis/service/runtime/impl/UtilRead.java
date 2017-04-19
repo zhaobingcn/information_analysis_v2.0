e011331c171264af0a16f87ee5fff9eb5b58d5a2
zhaobingcn <zhaobingcn@hotmail.com>
2017-04-19 23:54:48 +0800

    121323133

---
+++ src/main/java/com/isa/analysis/service/runtime/impl/UtilRead.java
@@ -0,0 +1,17 @@
+package com.isa.analysis.service.runtime.impl;
+
+import java.io.*;
+
+/**
+ * @author zhaobing
+ */
+public class UtilRead {
+    //读文件，变成流文件
+    public BufferedReader getBufferedReaderForJson(String file) throws FileNotFoundException {
+        File inFilePath = new File(file);
+        FileInputStream fileInputStream = new FileInputStream(inFilePath);
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader read = new BufferedReader(inputStreamReader);
+        return read;
+    }
+}
