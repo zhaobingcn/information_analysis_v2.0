9e766c333c6e2633c436d5a1f2fdb8aa468d9933
zhaobingcn <zhaobingcn@hotmail.com>
2017-05-01 23:40:06 +0800

    添加了社区发现的接口

--- src/main/java/com/isa/analysis/sdn/cypherquery/CypherQueryForSdn.java
+++ src/main/java/com/isa/analysis/sdn/cypherquery/CypherQueryForSdn.java
@@ -8,5 +8,4 @@ public class CypherQueryForSdn {
     String a = "MATCH (k:Keyword) WHERE id(k) = 42 CALL apoc.path.subgraphAll(k, {maxLevel:2, relationshipFilter:\"similar\", bfs:true, filterStartNode:false, limit:1000}) YIELD relationships with relationships as rel\n" +
             "return extract(r in rel | startNode(r)), extract(r in rel | endNode(r))";
 
-
 }
