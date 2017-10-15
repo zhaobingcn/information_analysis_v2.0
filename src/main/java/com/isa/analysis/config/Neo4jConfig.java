1880c445a6fcce2482a4aa279e72ff7af16dafd9
zhaobingcn <zhaobingcn@hotmail.com>
2017-10-15 18:00:38 +0800

    再次修改了部署的问题

--- src/main/java/com/isa/analysis/config/Neo4jConfig.java
+++ src/main/java/com/isa/analysis/config/Neo4jConfig.java
@@ -9,6 +9,9 @@ import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by zhaobing on 2016/12/30.
  */
@@ -24,12 +27,25 @@ public class Neo4jConfig extends Neo4jConfiguration {
      * `服务器模式连接
      * @return
      */
+
+
+
     @Bean
     public org.neo4j.ogm.config.Configuration getEmbeddedConfiguration() {
+
+        Properties prop = new Properties();
+        try {
+            prop.load(this.getClass().getResourceAsStream("/neo4j.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String uri = "http://" +prop.get("neo4j.name")+ ":" +prop.get("neo4j.password")+ "@localhost:7474";
+
         org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
         config.driverConfiguration()
                 .setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver")
-                .setURI("http://neo4j:654321@localhost:7474");
+                .setURI(uri);
         return config;
     }
 
